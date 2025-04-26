ecc0b112b971ded333a13b1d55c4ef46865cdeb7
NewOld21 <kshandy0221@gmail.com>
2025-04-27 00:12:44 +0900

    add : 05_이진탐색_NewOld21

--- 5) 이진탐색 (Binary Search)/세헌/BOJ_1939_중량제한.py
+++ 5) 이진탐색 (Binary Search)/세헌/BOJ_1939_중량제한.py
@@ -1,5 +1,5 @@
 import sys
-import heapq
+from collections import deque
 N, M = map(int, sys.stdin.readline().split())
 weight = [[0]*(N+1) for _ in range(N+1)]
 for _ in range(M) :
@@ -14,14 +14,14 @@ for _ in range(M) :
         
 
 f1, f2 = map(int, sys.stdin.readline().split())
-q = []
+q = deque()
 
-heapq.heappush(q,[-sys.maxsize,f1])
+q.append([-sys.maxsize,f1])
 visited = [0] * (N+1)
 visited[f1] = 1
 mx = 0
 while q :
-    w, e = heapq.heappop(q)
+    w, e = q.popleft()
     w = -w
     if e == f2 :
         if mx < w :
@@ -39,3 +39,82 @@ while q :
             if l>mx:
                 q.append([-l,i])
 print(mx)
+
+
+
+
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for i in range(m):
+    x,y,c = map(int, input().split())
+    graph[x].append((y,c))
+    graph[y].append((x,c))
+start, end = map(int, input().split())
+def bfs():
+    que = deque()
+    que.append(graph[start][0])
+    que.append(graph[start][1])
+    max_cost = 0
+    while que:
+        t, cost = que.popleft()
+        if cost >= max_cost:
+            que.append(graph[t][0])
+            que.append(graph[t][1])
+            max_cost = cost
+        if t == end:
+                break
+    return max_cost
+print(bfs())
+
+
+
+
+
+
+
+
+
+from collections import defaultdict, deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+adj = defaultdict(list)
+weights = list()
+for _ in range(m):
+    a, b, c  = map(int, input().split())
+    adj[a].append((c, b))
+    adj[b].append((c, a))
+    if c not in weights:
+        weights.append(c)
+s, e = map(int, input().split())
+weights.sort()
+
+left = 0
+right = len(weights) - 1
+answer = 0
+while (left <= right):
+    mid = (left + right) // 2
+    avbw = weights[mid]
+    queue = deque()
+    queue.append(s)
+    visited = [False] * (n+1)
+    visited[s] = True
+    flag = False
+    while queue:
+        v = queue.popleft()
+        if v == e:
+            answer = max(answer, avbw)
+            flag = True
+            break
+        for c, nv in adj[v]:
+            if not visited[nv] and c <= avbw:
+                if nv != e: visited[nv] = True
+                queue.append(nv)
+    if not flag: right -= 1
+    else: left += 1
+print(answer)
