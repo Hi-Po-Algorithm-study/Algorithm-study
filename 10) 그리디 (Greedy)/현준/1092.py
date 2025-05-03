3512a4b0b4df46ea83d8f3f38fb9755536e615bc
hjun813 <hyeonjun0899@gmail.com>
2025-05-03 23:51:25 +0900

    add : 0_기초문제_1011.py

--- 10) 그리디 (Greedy)/현준/1092.py
+++ 10) 그리디 (Greedy)/현준/1092.py
@@ -5,8 +5,6 @@ N = int(input())
 crane = list(map(int, input().split()))
 M = int(input())
 boxWeight = list(map(int, input().split()))
-moving = [0 for _ in range(M)]
-craneCanMove = [0 for _ in range(N)]
 crane.sort(reverse=True)
 boxWeight.sort(reverse=True)
 idx = 0
@@ -17,18 +15,21 @@ print(boxWeight)
 if boxWeight[0] > crane[0]:
     print(-1)
 else:
+    boxIdx = 0
+    craneIdx = 0
+    moving = [0 for _ in range(M)]
     while True:
-        day += 1
-        for i in range(N):
-            for j in range(len(boxWeight)):
-                if boxWeight[i] <= crane[j]:
-                    boxWeight.remove(boxWeight[i])
-                    i -= 1
-                    break
-
+        craneCanMove = [0 for _ in range(N)]
+        
+        while True:
+            if boxWeight[boxIdx] <= crane[craneIdx]:
+                moving[boxIdx] = 1
+                craneCanMove[craneIdx] = 1
+            
 
 # while True:
 #     for j in range(N-1, -1, -1):
 
-#         for i in range(M-1, -1, -1):
+#         for
+#  i in range(M-1, -1, -1):
 #             if moving
