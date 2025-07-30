67de060e0207b0e708a76aea507d5b4ec79f4422
NewOld21 <kshandy0221@gmail.com>
2025-07-30 23:19:13 +0900

    ADD : 02_재귀_08_BFS,DFS_NewOld21

---
+++ 2) 재귀 (Recursion)/세헌/BOJ_11729_하노이_탑_이동_순서.py
@@ -0,0 +1,18 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+def Hanoi(n, x, y) :
+    if n>1 :
+        Hanoi(n-1, x, 6-x-y)
+    print(x,y)
+    if n>1 :
+        Hanoi(n-1, 6-x-y, y)
+
+
+print(2**N-1)
+Hanoi(N,1,3)
+
+
+
+
